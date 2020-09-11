06719ac4faa6f3271b2a666adf086adf7f6156f2
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-09-10 22:14:23 -0700

    Updated project code for part 2

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Benjamin Garcia on 9/10/20.
+//  Copyright © 2020 Benjamin Garcia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
